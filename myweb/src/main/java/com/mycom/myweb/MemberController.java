2120a5ead9097137cb0e00d099686fac34f649ae
hsh9454 <hsh9454@nate.com>
2026-01-28 17:11:43 +0900

    DB 조작 테스트 성공

--- myweb/src/main/java/com/mycom/myweb/MemberController.java
+++ myweb/src/main/java/com/mycom/myweb/MemberController.java
@@ -24,16 +24,16 @@ public class MemberController {
 	
 	@GetMapping("/insert") 
 	public String insertMember(@RequestParam("id") int id, @RequestParam("name") String name, Model model) {
-	    // 1. 여기서 진짜 DB에 넣으라고 일을 시켜야 합니다!
+	  
 	    memberService.insertTest(id, name); 
 	    
 	    model.addAttribute("msg", name + " 등록 성공!");
 	    return "showResult";
 	}
 
-	@GetMapping("/update") // 소문자로 맞추는 게 편합니다! 
+	@GetMapping("/update") 
 	public String updateMember(@RequestParam("id") int id, @RequestParam("name") String name, Model model) {
-	    // 2. 여기서 진짜 DB를 수정하라고 일을 시켜야 합니다!
+	   
 	    memberService.updateTest(id, name);
 	    
 	    model.addAttribute("msg", name + " 수정 성공!");
@@ -42,7 +42,7 @@ public class MemberController {
 
 	@GetMapping("/delete") 
 	public String deleteMember(@RequestParam("id") int id, Model model) {
-	    // 3. 여기서 진짜 DB를 삭제하라고 일을 시켜야 합니다!
+	   
 	    memberService.deleteTest(id);
 	    
 	    model.addAttribute("msg", "ID " + id + " 삭제 성공!");
