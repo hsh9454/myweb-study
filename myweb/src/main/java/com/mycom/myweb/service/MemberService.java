d4f1dc3bf6e93be65d617913b1bccdd8292b2675
hsh9454 <hsh9454@nate.com>
2026-01-28 16:30:43 +0900

    Han 등록 시도!

--- myweb/src/main/java/com/mycom/myweb/service/MemberService.java
+++ myweb/src/main/java/com/mycom/myweb/service/MemberService.java
@@ -2,5 +2,16 @@ package com.mycom.myweb.service;
 
 public interface MemberService {
 	public String getDbName();
+	
+	void register(int id, String name);
+	void modify(int id, String name);
+	void remove(int id);
+	
+	
+	
+	
+	
+	
+	
 
 }
--- myweb/src/main/java/com/mycom/myweb/service/MemberServiceImpl.java
+++ myweb/src/main/java/com/mycom/myweb/service/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.mycom.myweb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.mycom.myweb.Mapper.MemberMapper;
 
 @Service
@@ -16,7 +17,20 @@ public class MemberServiceImpl implements MemberService {
 		
 	}
 	
+	@Override
+	public void register(int id, String name) {
+		memberMapper.insertTest(id, name);
+	}
 	
+	@Override
+	public void modify(int id, String name) {
+		memberMapper.updateTest(id, name);
+	}
+	
+	@Override
+	public void remove(int id) {
+		memberMapper.deleteTest(id);
+	}
 	
 }
 
