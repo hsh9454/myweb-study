0e8666509b273e6fab8dd881a7c4fa9a8828fee8
hsh9454 <hsh9454@nate.com>
2026-01-28 11:17:26 +0900

    fix: @RequestParam 추가 및 JSP 메시지 출력 구현 완료

--- myweb/src/main/java/com/mycom/myweb/MemberController.java
+++ myweb/src/main/java/com/mycom/myweb/MemberController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.mycom.myweb.service.MemberService;
 
 @Controller
@@ -21,20 +23,20 @@ public class MemberController {
 	}
 	
 	@GetMapping("/insert") 
-	public String insertMember(String name, Model model) {
+	public String insertMember(@RequestParam("name") String name, Model model) {
 		model.addAttribute("msg", name + " 등록 시도!");
 		return "showResult";
 	}
 	
 	@GetMapping("/Update") 
-	public String updateMember(String name, Model model) {
+	public String updateMember(@RequestParam("name") String name, Model model) {
 		model.addAttribute("msg", name + " 수정 시도!");
 		return "showResult";
 	
 	}
 	
 	@GetMapping("/delete") //딜리트
-	public String deleteMember(String name, Model model) {
+	public String deleteMember(@RequestParam("name") String name, Model model) {
 		model.addAttribute("msg", name + " 삭제 시도!");
 		return "showResult";
 		
--- myweb/src/main/webapp/WEB-INF/views/showResult.jsp
+++ myweb/src/main/webapp/WEB-INF/views/showResult.jsp
@@ -4,13 +4,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title> 01-27 공부 일지 </title>
+<title> 01-28 공부 일지 </title>
 </head>
 <body>
-      <h1>DB 테이블안에  "한승훈"이라는 문자열 데이터 넣고 웹 어플리케이션에서 불러와서 화면에 나타내기 </h1>
+      <h1>pring Controller에 C, U, D 주소(엔드포인트) 만들기</h1>
       <hr>
-      <h1>반드시 JSP - 컨트롤러 - 서비스(서비스 임플 필수) - mapper 구조 사용 </h1>
+      <h1>Git을 사용해 각 단계를 명확한 메시지로 커밋하고 푸시하기</h1>
       <hr>
-      <h3>DB에서 가져온 이름: <span style="color: red;">${name}</span></h3>
+     
+      <h2>결과 메시지: ${msg}</h2>
 </body>
 </html>
